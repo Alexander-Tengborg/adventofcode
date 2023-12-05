836abdd5e1ca10bba24b7e5c091dfb3d3403c354
Alexander-Tengborg <Landervich@Gmail.com>
2023-12-05 22:03:39 +0100

    Day 05 optimized and cleaned up
    day05_2.py now contains a solution that is done near instantly, without using any threads. Still need to clean up other files, but day05_1.py and day05_2.py are looking good

--- 2023/05/day05_1.py
+++ 2023/05/day05_1.py
@@ -1,57 +1,69 @@
-import re
+file = open("day05_data.txt", "r")
 
-#file = open("day05_data.txt", "r")
-file = open("W:\\adventofcode\\2023\\05\\day05_data.txt", "r")
-
-lowest_location = 0
-
+# Will contain all of our seeds
 seeds = []
 
-all_maps = {}
+# Will contain the names of each map type, and a dict of every single map type and their maps (respectively)
 map_types = []
+all_maps = {}
 
+# The last map type we added data to
 last_map_type = ""
 
+# Read and parse all the data
 for line in file.readlines():
+    
+    # Empty line, skip
     if not line.strip():
         continue
 
+    # Saves all our seeds into a variable
     if line.startswith("seeds:"):
         seeds = [int(seed) for seed in line.replace("seeds:", '').strip().split(' ')]
         continue
 
+    # If the line contains the word "map", a new type of map has been found
     if (index := line.find("map")) > 0:
         last_map_type = line[0:index-1]
         all_maps[last_map_type] = []
         map_types.append(last_map_type)
+        
         continue
 
+    # Found a map, parse and save the data
     map = [int(data) for data in line.strip().split(' ')]
-
     all_maps[last_map_type].append(map)
 
+# The path of a seed to its location
 seed_to_location = []
-last_number = 0
 
+current_number = 0
+
+# Loops through each seed
 for i, seed in enumerate(seeds):
     seed_to_location.append([seed])
-    last_number = seed
+    current_number = seed
 
+    # Loops through each map type
     for map_type in map_types:
         maps = all_maps[map_type]
         matched_map = False
 
+        # Loops through each map in a map type
         for map in maps:
             dest_range = map[0]
             src_range = map[1]
             range_len = map[2]
 
-            if(src_range <= last_number <= (src_range + range_len - 1)):
-                last_number += dest_range - src_range
+            # Convert the current number if its within the map range
+            if(src_range <= current_number <= (src_range + range_len - 1)):
+                current_number += dest_range - src_range
                 break
 
-        seed_to_location[i].append(last_number)
+        # Save the current converted number and move on to the next map type
+        seed_to_location[i].append(current_number)
 
+# Gets the lowest possible location number
 min_location = min([v[-1] for v in seed_to_location])
 
 print(min_location)
--- 2023/05/day05_2.py
+++ 2023/05/day05_2.py
@@ -1,69 +1,107 @@
-import re
+file = open("day05_data.txt", "r")
 
-############### DOES NOT WORK
-############### DOES NOT WORK
-# (MIGHT ACTUALLY WORK BUT CRASHES DUE TO USING TOO MUCH MEMORY)
+# Will contain all of our seed ranges
+seed_ranges = []
 
-#file = open("day05_data.txt", "r")
-file = open("W:\\adventofcode\\2023\\05\\day05_data.txt", "r")
-
-lowest_location = 0
-
-seeds = []
-
-all_maps = {}
+# Will contain the names of each map type, and a dict of every single map type and their maps (respectively)
 map_types = []
+all_maps = {}
 
+# The last map type we added data to
 last_map_type = ""
 
+# Read and parse all the data
 for line in file.readlines():
+
+    # Empty line, skip
     if not line.strip():
         continue
 
+    # Saves all our seeds into an array of tuples, where each tuple has 2 values (start and stop - range values)
     if line.startswith("seeds:"):
-        seed_ranges = [int(seed) for seed in line.replace("seeds:", '').strip().split(' ')]
-        range_pairs = int(len(seed_ranges)/2)
+        seed_ranges_raw = [int(seed) for seed in line.replace("seeds:", '').strip().split(' ')]
+        
+        range_pairs = int(len(seed_ranges_raw)/2)
         for i in range(range_pairs):
-            start_range = seed_ranges[i*2]
-            stop_range = seed_ranges[i*2] + seed_ranges[i*2+1]
-            seeds.extend([*range(start_range, stop_range)])
+            start_range = seed_ranges_raw[i*2]
+            stop_range = start_range + seed_ranges_raw[i*2+1] - 1
+
+            seed_ranges.append((start_range, stop_range))
         continue
 
+    # If the line contains the word "map", a new type of map has been found
     if (index := line.find("map")) > 0:
         last_map_type = line[0:index-1]
         all_maps[last_map_type] = []
         map_types.append(last_map_type)
+
         continue
 
-    map = [int(data) for data in line.strip().split(' ')]
+    # Split the map data
+    map_data = line.strip().split(' ')
 
+    # Save the map in a format that will be easy to use later
+    start_range = int(map_data[1])
+    end_range = int(map_data[1]) + int(map_data[2]) - 1
+    conversion = int(map_data[0]) - int(map_data[1])
+
+    map = [start_range, end_range, conversion]
+
+    # Save the found/created map
     all_maps[last_map_type].append(map)
 
-seed_to_location = []
-last_number = 0
+# Stores all the possible location ranges
+possible_location_ranges = seed_ranges
 
-print("Parsing done")
+# Loops through each map type
+for name, maps in all_maps.items():
 
-for i, seed in enumerate(seeds):
-    seed_to_location.append([seed])
-    last_number = seed
-    for map_type in map_types:
-        maps = all_maps[map_type]
-        matched_map = False
+    # Loops through every saved possible location value
+    for key, current_range in enumerate(possible_location_ranges):
 
+        # Loops through every map of the current type
         for map in maps:
-            dest_range = map[0]
-            src_range = map[1]
-            range_len = map[2]
+            # Add this number to a range to convert the range using the current maps value
+            convert_number = map[2]
 
-            if(src_range <= last_number <= (src_range + range_len - 1)):
-                last_number += dest_range - src_range
-                break
+            # If the range is completely inside the map, convert the entire range
+            if current_range[0] >= map[0] and current_range[1] <= map[1]:
+                possible_location_ranges[key] = (current_range[0] + convert_number,
+                                                 current_range[1] + convert_number)
+            
+            # If the map is completely inside the range, split the range into 3 parts, convert the middle one
+            elif current_range[0] < map[0] and current_range[1] > map[1]:
+                possible_location_ranges[key] = (map[0] + convert_number, map[1] + convert_number)
 
-        seed_to_location[i].append(last_number)
+                first_range = (current_range[0], map[0]-1)
+                second_range = (map[1]+1, current_range[1])
 
-min_location = min([v[-1] for v in seed_to_location])
+                possible_location_ranges.append(first_range)
+                possible_location_ranges.append(second_range)
 
-print(min_location)
+            # If the beginning of the range is inside the map, but the end is outside, split into 2 parts,
+            # convert the first part that is inside the map
+            elif (map[1] >= current_range[0] >= map[0]) and current_range[1] > map[1]:
+                possible_location_ranges[key] = (current_range[0] + convert_number, map[1] + convert_number)
+                
+                second_range = (map[1] + 1, current_range[1])
+
+                possible_location_ranges.append(second_range)
+
+            # If the beginning of the range is outside the map, but the end of the range is outside, split into
+            # 2 parts and convert the second part of the range that is inside the map
+            elif current_range[0] < map[0] and (map[0] <= current_range[1] <= map[1]):
+                possible_location_ranges[key] = (map[0] + convert_number, current_range[1] + convert_number)
+
+                first_range = (current_range[0], map[0] - 1)
+
+                possible_location_ranges.append(first_range)
+
+# Gets the minimum location value out of all the possible location ranges
+# The first index contains the beginning (minimum) of a range, so we're using that instead of
+# The second index that instead contains the end (maximum) of a range
+minimum_value = min(possible_location_ranges, key=lambda x:x[0])[0]
+
+print(minimum_value)
 
 file.close()
